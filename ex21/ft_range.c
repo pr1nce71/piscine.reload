878f79489f5b2fb69dcfcf24c5d9d955c8178f3b
Yevhenii Kaharlytskyi <yevkahar@c1r2s1.42wolfsburg.de>
2024-11-08 18:50:38 +0100

    not fin

---
+++ ex21/ft_range.c
@@ -0,0 +1,23 @@
+
+#include <unistd.h>
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*range;
+	int	i;
+
+	if (min >= max)
+		return (NULL);
+	range = (int *)malloc(sizeof(int) * (max - min));
+	if (range == NULL)
+		return (NULL);
+	i = 0;
+	while (min < max)
+	{
+		range[i] = min;
+		min++;
+		i++;
+	}
+	return (range);
+}
