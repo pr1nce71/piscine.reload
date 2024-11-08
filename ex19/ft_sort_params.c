5ac98f7a538b0466be32d2c234e9ee83ec6dd307
Yevhenii Kaharlytskyi <yevkahar@c1r3s1.42wolfsburg.de>
2024-11-08 16:00:05 +0100

    fin19

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -18,7 +18,7 @@ void ft_putstr(char *str)
 
 int ft_strcmp(char *s1, char *s2)
 {
-    unsigned int i;
+    int i;
 
     i = 0;
     while(s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
@@ -28,16 +28,40 @@ int ft_strcmp(char *s1, char *s2)
     return(s1[i] - s2[i]);
 }
 
-void ft_swap(int *a, int *b)
+void ft_swap(char **a, char **b)
 {
     int swap;
 
-    swap = *a;
-    *a = *b;
-    *b = swap;
+    swap = **a;
+    **a = **b;
+    **b = swap;
 }
 
 int main(int ac, char **av)
 {
-    
+    int j;
+    int x;
+
+    x = 1;
+    while(x < ac - 1)
+    {
+        j = 1;
+        while(j < ac - 1)
+        {
+            if(ft_strcmp(av[j] , av[j + 1]) > 0)
+            {
+                ft_swap(&av[j], &av[j + 1]);
+            }
+            j++;
+        }
+        x++;
+    }
+    j = 1;
+    while(j < ac)
+    {
+        ft_putstr(av[j]);
+        ft_putchar('\n');
+        j++;
+    }
+    return(0);
 }
